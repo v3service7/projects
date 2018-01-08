19e16fdde672be043f37a8812b2c1f03819cc3fa
v3xperts <business@v3xperts.com>
2018-01-08 10:23:11 +0530

    dating code remove from this account

--- Dating/app/src/app/global.ts
+++
@@ -1,3 +0,0 @@
-export const imageUrl: string = 'http://34.209.114.118:4005/uploads/';
-export const url: string = 'http://34.209.114.118:4005/';
-export const frontUrl: string = 'http://34.209.114.118:3000/';
