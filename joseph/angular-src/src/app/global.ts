6a2198525825662a3d9c53a6381b8cd3695019c4
v3xperts <business@v3xperts.com>
2018-05-10 15:57:38 +0530

    trade delete

--- joseph/angular-src/src/app/global.ts
+++
@@ -1,4 +0,0 @@
-export const url: string = 'http://18.221.47.77:3000/';
-//export const url: string = '/';
-//export const url: string = 'http://localhost:3000/';
-export const imageUrl: string = '/uploads/';
